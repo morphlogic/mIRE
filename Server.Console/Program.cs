7673cf1fd5a4104996a3dff49b53c261072d82f2
morphlogic <sam@samshaw.us>
2024-03-24 11:55:06 -0500

    Added basic Serilog + Microsoft ILogger support in Server.Console
    Needs refactoring, IoC integration, and support in Server.Core

--- Server.Console/Program.cs
+++ Server.Console/Program.cs
@@ -1,7 +1,24 @@
 ﻿using DryIoc;
+using Microsoft.Extensions.Logging;
 using mIRE.Server.Core;
+using Serilog;
 using System.Net;
 using System.Net.Sockets;
+
+
+//  START:  Logging setup
+
+var serilogLogger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("log.txt")
+    .CreateLogger();
+
+var loggerFactory = new LoggerFactory().AddSerilog(serilogLogger);
+
+Microsoft.Extensions.Logging.ILogger microsoftLogger = loggerFactory.CreateLogger("Log");
+
+//  END:    Logging setup
 
 //var listener = new TcpListener(Configuration.Host, Configuration.Port);
 
@@ -13,7 +30,13 @@ var listener = new TcpListener(host, port);
 
 listener.Start();
 
-Console.WriteLine($"mIRE is listening @ {host} on port {port}...");
+//  S:  Logging test
+
+//Console.WriteLine($"mIRE is listening @ {host} on port {port}...");
+
+microsoftLogger.LogInformation("mIRE is listening @ {host} on port {port}...", host, port);
+
+//  E:  Logging test
 
 var container = LiveIocRegistrar.Container;
 
